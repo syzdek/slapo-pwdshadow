0ef0b962b76239a152dca7f6548bed18b2fe7666
David M. Syzdek <david@syzdek.net>
2023-04-25 09:47:29 -0800

    pwdshadow: renaming variables 'psm' to 'dat'

--- pwdshadow.c
+++ pwdshadow.c
@@ -171,31 +171,31 @@ pwdshadow_get_attr_time(
 static int
 pwdshadow_get_mods(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm );
+      pwdshadow_data_t *               dat );
 
 
 static int
 pwdshadow_get_mods_bool(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm );
+      pwdshadow_data_t *               dat );
 
 
 static int
 pwdshadow_get_mods_exists(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm );
+      pwdshadow_data_t *               dat );
 
 
 static int
 pwdshadow_get_mods_integer(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm );
+      pwdshadow_data_t *               dat );
 
 
 static int
 pwdshadow_get_mods_time(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm );
+      pwdshadow_data_t *               dat );
 
 
 static int
@@ -868,68 +868,68 @@ pwdshadow_get_attr_time(
 int
 pwdshadow_get_mods(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm )
+      pwdshadow_data_t *               dat )
 {
    switch(mods->sml_op)
    {
-      case LDAP_MOD_DELETE:  return(psm->op = PWDSHADOW_OP_DELETE);
-      case LDAP_MOD_ADD:     return(psm->op = PWDSHADOW_OP_ADD);
+      case LDAP_MOD_DELETE:  return(dat->op = PWDSHADOW_OP_DELETE);
+      case LDAP_MOD_ADD:     return(dat->op = PWDSHADOW_OP_ADD);
       case LDAP_MOD_REPLACE: break;
-      default:               return(psm->op = PWDSHADOW_OP_UNKNOWN);
+      default:               return(dat->op = PWDSHADOW_OP_UNKNOWN);
    };
-   return(psm->op = ((mods->sml_numvals < 1) ? PWDSHADOW_OP_DELETE : PWDSHADOW_OP_ADD));
+   return(dat->op = ((mods->sml_numvals < 1) ? PWDSHADOW_OP_DELETE : PWDSHADOW_OP_ADD));
 }
 
 
 int
 pwdshadow_get_mods_bool(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm )
+      pwdshadow_data_t *               dat )
 {
    int rc;
-   if ((rc = pwdshadow_get_mods(mods, psm)) != PWDSHADOW_OP_ADD)
+   if ((rc = pwdshadow_get_mods(mods, dat)) != PWDSHADOW_OP_ADD)
       return(rc);
-   psm->new = 1;
+   dat->new = 1;
    if ((pwdshadow_verify_attr_syntax(mods->sml_desc, "1.3.6.1.4.1.1466.115.121.1.7")))
-      psm->new = pwdshadow_parse_bool(&mods->sml_values[0]);
-   return(psm->op);
+      dat->new = pwdshadow_parse_bool(&mods->sml_values[0]);
+   return(dat->op);
 }
 
 
 int
 pwdshadow_get_mods_exists(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm )
+      pwdshadow_data_t *               dat )
 {
    int rc;
-   if ((rc = pwdshadow_get_mods(mods, psm)) != PWDSHADOW_OP_ADD)
+   if ((rc = pwdshadow_get_mods(mods, dat)) != PWDSHADOW_OP_ADD)
       return(rc);
-   psm->new = 1;
-   return(psm->op);
+   dat->new = 1;
+   return(dat->op);
 }
 
 
 int
 pwdshadow_get_mods_integer(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm )
+      pwdshadow_data_t *               dat )
 {
    int rc;
    int i;
 
-   if ((rc = pwdshadow_get_mods(mods, psm)) != PWDSHADOW_OP_ADD)
+   if ((rc = pwdshadow_get_mods(mods, dat)) != PWDSHADOW_OP_ADD)
       return(rc);
 
    // process attribute as Integer
-   psm->cur = 0;
+   dat->cur = 0;
    if ((pwdshadow_verify_attr_syntax(mods->sml_desc, "1.3.6.1.4.1.1466.115.121.1.27")))
    {
       i = 0;
       lutil_atoi(&i, mods->sml_values[0].bv_val);
-      psm->new = i;
+      dat->new = i;
    };
 
-   return(psm->op);
+   return(dat->op);
 }
 
 
@@ -937,27 +937,27 @@ pwdshadow_get_mods_integer(
 int
 pwdshadow_get_mods_time(
       Modifications *                  mods,
-      pwdshadow_data_t *               psm )
+      pwdshadow_data_t *               dat )
 {
    int      rc;
    time_t   t;
    int      i;
 
-   if ((rc = pwdshadow_get_mods(mods, psm)) != PWDSHADOW_OP_ADD)
+   if ((rc = pwdshadow_get_mods(mods, dat)) != PWDSHADOW_OP_ADD)
       return(rc);
 
-   psm->new = 0;
+   dat->new = 0;
 
    // process attribute as Generalized Time
    if ((pwdshadow_verify_attr_syntax(mods->sml_desc, "1.3.6.1.4.1.1466.115.121.1.24")))
    {
       if ((t = pwdshadow_parse_time(mods->sml_values[0].bv_val)) == ((time_t)-1))
-         return(psm->op);
+         return(dat->op);
       t /= 60; // convert to minutes
       t /= 60; // convert to hours
       t /= 24; // convert to days
-      psm->new = (int)t;
-      return(psm->op);
+      dat->new = (int)t;
+      return(dat->op);
    };
 
    // process attribute as Integer
@@ -965,11 +965,11 @@ pwdshadow_get_mods_time(
    {
       i = 0;
       lutil_atoi(&i, mods->sml_values[0].bv_val);
-      psm->new = i;
-      return(psm->op);
+      dat->new = i;
+      return(dat->op);
    };
 
-   return(psm->op);
+   return(dat->op);
 }
 
 
