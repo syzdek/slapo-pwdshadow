2bd7a2d3abd7d977debc03d1b051478d0fe5e7f5
David M. Syzdek <david@syzdek.net>
2023-04-28 13:52:20 -0800

    pwdshadow: removing global variable ad_shadowLastChange

--- pwdshadow.c
+++ pwdshadow.c
@@ -260,7 +260,6 @@ static slap_overinst pwdshadow;
 static AttributeDescription *       ad_pwdChangedTime       = NULL;
 static AttributeDescription *       ad_pwdEndTime           = NULL;
 static AttributeDescription *       ad_shadowExpire         = NULL;
-static AttributeDescription *       ad_shadowLastChange     = NULL;
 
 
 // external internal descriptions
@@ -783,8 +782,7 @@ pwdshadow_db_init(
       slap_str2ad("pwdEndTime", &ps->ps_ad_pwdEndTime, &text);
    if ((ps->ps_ad_shadowExpire = ad_shadowExpire) == NULL)
       slap_str2ad("shadowExpire", &ps->ps_ad_shadowExpire, &text);
-   if ((ps->ps_ad_shadowLastChange = ad_shadowLastChange) == NULL)
-      slap_str2ad("shadowLastChange", &ps->ps_ad_shadowLastChange, &text);
+   slap_str2ad("shadowLastChange", &ps->ps_ad_shadowLastChange, &text);
    if ((ps->ps_ad_userPassword = slap_schema.si_ad_userPassword) == NULL)
       slap_str2ad("userPassword", &ps->ps_ad_userPassword, &text);
 
@@ -1040,7 +1038,6 @@ pwdshadow_initialize( void )
    slap_str2ad("pwdChangedTime",       &ad_pwdChangedTime,     &text);
    slap_str2ad("pwdEndTime",           &ad_pwdEndTime,         &text);
    slap_str2ad("shadowExpire",         &ad_shadowExpire,       &text);
-   slap_str2ad("shadowLastChange",     &ad_shadowLastChange,   &text);
 
    pwdshadow.on_bi.bi_type             = "pwdshadow";
    pwdshadow.on_bi.bi_flags            = SLAPO_BFLAG_SINGLE;
